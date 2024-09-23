30ac48f8cb9da94db6fdb743d68a047c1e64f286
Magnus <magnus.h@wemail.no>
2024-09-24 01:16:44 +0200

    created the front-page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import HomePage from '/components/HomePage'
+import HomePage from './components/HomePage'
 
 function App() {
 
   return (
     <>
-      <div>
-        <HomePage/>
+      <div className='first_page'>
+        <div className='topmenu'>
+          <nav>
+            <a className='topmenu-item'
+              src='https://magnusgit1.github.io/quiz-game/'>
+              <u>Home</u>
+            </a>
+            <a className='topmenu-item'
+              //src= source to about-page
+              ><u>About</u></a>
+            <a className='topmenu-item'
+              //src= source to how-to-play page
+              ><u>How to play</u>
+            </a>
+            <img src="https://img.freepik.com/premium-vector/quiz-logo-with-speech-bubble-icon_149152-812.jpg"/>
+          </nav>
+        </div>
+        <div className='home_page'>
+          <HomePage/>
+        </div>
       </div>
     </>
   )
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,34 +1,22 @@
-
+import "./HomePage.css";
 const HomePage = () => {
 
 
     return (
         <div className='main'>
-            <div className='nav-bar'>
-                <nav>
-                    <a className='topmenu'
-                        src='https://magnusgit1.github.io/quiz-game/'>
-                        <u>Home</u>
-                    </a>
-                    <a className='topmenu'
-                        //src= source to how-to-play page
-                        ><u>How To Play</u>
-                    </a>
-                </nav>
-            </div>
-            <div className='welcome-section'>
+            <div className='welcome_section'>
                 <h1>Welcome to Quizorama</h1>
-                <div className='get-started'>
+                <div className='get_started'>
                     <h2>How would you like to play?</h2>
-                    <div className='get-started-btns'>
-                        <button className='login-btn'>
-                            Login as existing user
+                    <div className='get_started_btns'>
+                        <button className='offline_btn'>
+                            Play Offline
                         </button>
-                        <button className='sign-in-btn'>
+                        <button className='sign_in_btn'>
                             Sign Up
                         </button>
-                        <button className='offline-btn'>
-                            Play Offline
+                        <button className='login_btn'>
+                            Login as existing user
                         </button>
                     </div>
                 </div>
