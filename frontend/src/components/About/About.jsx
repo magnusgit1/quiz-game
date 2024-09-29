d037cd3f5dadd795fa7b9d5b8724217ba96e3358
Magnus <magnus.h@wemail.no>
2024-09-29 15:05:07 +0200

    changes to About and HowToPlay-sections

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,9 +1,39 @@
 
+import './About.css';
+
 function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>This page will be about something</p>
+        <div className='main_about'>
+        <div className='main_init_about'>
+            <h1>About</h1>
+            <p>
+                I am a student at the University of Oslo, working towards my degree in Informatics and Computer Science.
+                In parallell with my degree, I am trying to apply my theoretical knowledge to create functional and real-life
+                applications. In this project specifically, i am focusing on applying my newly developed (and partly self-learned)
+                front-end skills, to create a simple quiz-game-website. I also learned alot about server-side programming, where
+                i implemented authentication-based login-functionality, including registering new accounts, storing high-scores and data
+                configuration aquired from the users with the use of jwt, dj-auth and rest-frameworks from Django in Python.
+                The learning curve has been rather steep, but i truly enjoy the process, and learn alot from all the mistakes and debugging
+                i stumble upon in the process. <br></br>
+
+                <h2><u>Tech stack</u></h2>
+                <h3>Frontend</h3>
+                <ul className='tech_stack'>
+                    <li>React with:</li>
+                    <ul>
+                        <li>HTML, CSS, Javascript (jsx) </li>
+                    </ul>
+                </ul>
+                <h3>Backend</h3>
+                <ul className='tech_stack'>
+                    <li>Python with:</li>
+                    <ul>
+                        <li>Django (dj-auth etc.) </li>
+                        <li>PostgreSQL</li>
+                    </ul>
+                </ul>
+            </p>
+        </div>
         </div>
     );
 }
--- frontend/src/components/HowToPlay/HowToPlay.jsx
+++ frontend/src/components/HowToPlay/HowToPlay.jsx
@@ -1,10 +1,47 @@
 
+import './HowToPlay.css'
+
 function HowToPlay() {
     
     return(
-        <div>
+        <div className='main_howtoplay'>
+        <div className='main_init_howtoplay'>
             <h1>How To play</h1>
-            <p>This page will show how the game works</p>
+            <p> 
+                The game is implemented in a way such that only registered players
+                will have their high-scored saved - and thus applicable to be a part of the leaderbord. <br></br> <br></br>
+
+                The game consists of a few mandatory parts - firstly, one needs to choose 
+                a category in which they want to test their knowledge. Secondly, the player needs 
+                to choose the difficulty of the quiz. This ranges between easy, which are rather trivial questions,
+                to hard, which requires more in-depth knowledge and/or interest in the field. <br></br> <br></br>
+
+                After both the category and the difficulty is chosen, the player will be redirected to the actual game.
+                Here the rules are simple - a question will appear on the top-side of the game-screen, and the player 
+                has a total of 10 seconds to decide which answer between the four options provided, they want to lock in.
+                After the 10 seconds, or if the player chooses to click the lock-in button early, the screen will display
+                which answer was correct, and grant the player points if their answer matched the correct one.
+                After all the questions have been answered, the player will be presented with their total score of the quiz. <br></br> <br></br>
+
+                In addition to getting points for correct answers, the players will also be rewarded points depending on how 
+                quickly they lock in their answer. 
+
+                The point system works as follows: <br></br>
+                <ul>
+                    <li>Correct answer grants 5 points</li>
+                    <li>Each second the player saves of the clock, will result in 1 additional point</li>
+                    <li>For example, if the answer is locked in only after the clock runs out, the player will be awarded 5 points</li>
+                    <li>If the player locks in when the timer shows 9 seconds remaining, they will be granted 5 + 9 = 14 points</li>
+                </ul>
+                <br></br>
+
+                There will also be separate leaderbords for the different difficulties, meaning 
+                one can hold the best score for the easy-mode, while another holds the best score of the medium-mode.
+                <br></br>
+                <br></br>
+                Good quizzing!
+            </p>
+        </div>
         </div>
     );
 }
